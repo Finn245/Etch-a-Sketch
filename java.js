6848335101cb3860512f66469d201c023a767246
Finn245 <aderahmenmanai@gmail.com>
2023-12-02 22:29:14 +0100

    Etch-a-Sketch

--- java.js
+++ java.js
@@ -3,61 +3,56 @@ const size = document.getElementById('size');
 const sizeValue = document.getElementById('sizevalue');
 const colorButton = document.querySelector('.colorMode');
 let isDrawing = false;
-
 let selectedColor = 'black';
 
 size.addEventListener('input', () => {
     sizeValue.innerHTML = `${size.value}*${size.value}`;
     flexContainer.innerHTML = '';
     flexContainer.style.backgroundColor = 'white';
+
     for (let i = 0; i < size.value; i++) {
         const row = document.createElement('div');
         row.classList.add("row");
+
         for (let j = 0; j < size.value; j++) {
             const box = document.createElement('div');
             box.classList.add('box');
             row.append(box);
-            Drawing(box);
-
-            colorButton.addEventListener('click',()=>{
-                Drawing(box);
-            })
-
-            clear(box);
-            rainbowDraw(box);
         }
+
         flexContainer.append(row);
     }
+
+    // Add event listener for the entire grid
+    flexContainer.addEventListener('mousedown', handleBoxMouseDown);
+    flexContainer.addEventListener('mousemove', handleBoxMouseMove);
+    flexContainer.addEventListener('mouseup', handleBoxMouseUp);
+    flexContainer.addEventListener('mouseleave', handleBoxMouseLeave);
 });
 
-function Drawing(box) {
-    
-    
+function handleBoxMouseDown(event) {
+    const box = event.target;
 
-        box.addEventListener('mousedown', () => {
-        
-            isDrawing = true;
-            box.style.backgroundColor = selectedColor;
-       
-    });
+    if (box.classList.contains('box')) {
+        isDrawing = true;
+        box.style.backgroundColor = selectedColor;
+    }
+}
 
-    box.addEventListener('mousemove', () => {
-        if (isDrawing) {
-            box.style.backgroundColor = selectedColor;
-        }
-    });
+function handleBoxMouseMove(event) {
+    const box = event.target;
 
-    box.addEventListener('mouseup', () => {
-        isDrawing = false;
-    });
+    if (isDrawing && box.classList.contains('box')) {
+        box.style.backgroundColor = selectedColor;
+    }
+}
 
-    flexContainer.addEventListener('mouseleave', () => {
-        isDrawing = false;
-        
-    });
+function handleBoxMouseUp() {
+    isDrawing = false;
+}
 
-    
-   
+function handleBoxMouseLeave() {
+    isDrawing = false;
 }
 
 const colorInput = document.querySelector(".colorPanel");
@@ -65,38 +60,47 @@ colorInput.addEventListener('input', (event) => {
     selectedColor = event.target.value;
 });
 
-
-function clear(box){
-    const clear = document.querySelector('.clear');
-clear.addEventListener('click', () => {
-    box.style.backgroundColor = 'white';
-});
-
+function clear() {
+    flexContainer.querySelectorAll('.box').forEach(box => {
+        box.style.backgroundColor = 'white';
+    });
 }
-function rainbowColor(){
-    const rainbowColors=['red','yellow','blue','green','black','purple'];
+
+function rainbowColor() {
+    const rainbowColors = ['red', 'yellow', 'blue', 'green', 'black', 'purple'];
     const randomColor = Math.floor(Math.random() * rainbowColors.length);
     return rainbowColors[randomColor];
 }
 
-function rainbowDraw(box){
-    rainbowButton=document.querySelector(".rainbow");
-    rainbowButton.addEventListener('click',()=>{
-        
-        box.addEventListener('mousedown',()=>{
-            isDrawing=true;
-            box.style.backgroundColor=rainbowColor();
-        })
-        box.addEventListener('mousemove', () => {
-            if (isDrawing) {
-                box.style.backgroundColor = rainbowColor();
-            }
-        });
-        box.addEventListener('mouseup',()=>{
-            isDrawing=false;
-        })
-        flexContainer.addEventListener('mouseleave',()=>{
-            isDrawing=false;
-        })
-    })
+function rainbowDraw() {
+    flexContainer.addEventListener('mousedown', (event) => {
+        const box = event.target;
+
+        if (box.classList.contains('box')) {
+            isDrawing = true;
+            box.style.backgroundColor = rainbowColor();
+        }
+    });
+
+    flexContainer.addEventListener('mousemove', (event) => {
+        const box = event.target;
+
+        if (isDrawing && box.classList.contains('box')) {
+            box.style.backgroundColor = rainbowColor();
+        }
+    });
+
+    flexContainer.addEventListener('mouseup', () => {
+        isDrawing = false;
+    });
+
+    flexContainer.addEventListener('mouseleave', () => {
+        isDrawing = false;
+    });
 }
+
+const clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click', clear);
+
+const rainbowButton = document.querySelector(".rainbow");
+rainbowButton.addEventListener('click', rainbowDraw);
